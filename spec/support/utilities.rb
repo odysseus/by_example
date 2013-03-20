16147a3a59c0e53fafdd94971cfb353e79697897
Ryan Case <rmcase@gmail.com>
2013-03-19 19:23:26 -0600

    Finished signing in and out

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,38 @@
 include ApplicationHelper
+
+def valid_signin(user)
+  fill_in "Email",      with: user.email
+  fill_in "Password",   with: user.password
+  click_button "Sign in"
+end
+
+def valid_signup()
+  fill_in "Name",         with: "Example User"
+  fill_in "Email",        with: "user@example.com"
+  fill_in "Password",     with: "foobar"
+  fill_in "Confirmation", with: "foobar"
+end
+
+RSpec::Matchers.define :flash_error do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :flash_success do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-success', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_title do |text|
+  match do |page|
+    page.should have_selector('title', text: text)
+  end
+end
+
+RSpec::Matchers.define :have_heading do |text|
+  match do |page|
+    page.should have_selector('h1', text: text)
+  end
+end
