b79eb621a100b8525a8b5175af5f2c75a4f73ef7
Ryan Case <rmcase@gmail.com>
2013-03-20 15:52:28 -0600

    Replaced all but layouts/application.html.erb with Haml files

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'unicorn-rails'
 gem 'faker'
 gem 'will_paginate'
 gem 'bootstrap-will_paginate'
+gem 'haml'
 
 group :development, :test do
 	gem 'rspec-rails'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,9 +28,11 @@ describe User do
 	it { should respond_to :password }
 	it { should respond_to :password_confirmation }
 	it { should respond_to :remember_token }
+  it { should respond_to :admin }
 	it { should respond_to :authenticate }
 
 	it { should be_valid }
+  it { should_not be_admin }
 
 	describe "When name is not present" do
 		before { @user.name = " " }
@@ -127,5 +129,14 @@ describe User do
   describe "remember token" do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
+  end
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
   end
 end
