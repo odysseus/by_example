c4f4a4434b4a99e58f98c95c6ab00fbc58f50ed1
Ryan Case <rmcase@gmail.com>
2013-03-25 22:49:20 -0600

    Changed 'microposts' to 'chirps' in a few locations

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,17 +4,17 @@ describe "Static pages" do
 
   subject { page }
 
-	shared_examples_for "all static pages" do
-		it { should have_heading(heading) }
-		it { should have_title(full_title(page_title)) }
-	end
+  shared_examples_for "all static pages" do
+    it { should have_heading(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
   describe "Home page" do
     before { visit root_path }
-		let(:heading)			{ 'Avian' }
-		let(:page_title)	{ '' }
-		it_should_behave_like "all static pages"
-		it { should_not have_selector 'title', text: '| Home' }
+    let(:heading)			{ 'Avian' }
+    let(:page_title)	{ '' }
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: '| Home' }
 
     describe "for signed in users" do
       let(:user) { FactoryGirl.create(:user) }
@@ -47,26 +47,26 @@ describe "Static pages" do
 
   describe "Help page" do
     before { visit help_path }
-		let(:heading)			{ 'Help' }
-		let(:page_title)				{ 'Help' }
-		it_should_behave_like "all static pages"
+    let(:heading)			{ 'Help' }
+    let(:page_title)				{ 'Help' }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
-		let(:heading)			{ 'About' }
-		let(:page_title) 			{ 'About' }
-		it_should_behave_like "all static pages"
+    let(:heading)			{ 'About' }
+    let(:page_title) 			{ 'About' }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
-		let(:heading)			{ 'Contact' }
-		let(:page_title)				{ 'Contact' }
-		it_should_behave_like "all static pages"
+    let(:heading)			{ 'Contact' }
+    let(:page_title)				{ 'Contact' }
+    it_should_behave_like "all static pages"
   end
 
-	it "should have the right links on the layout" do
+  it "should have the right links on the layout" do
     visit root_path
     click_link "About"
     page.should have_selector 'title', text: full_title('About')
@@ -80,5 +80,5 @@ describe "Static pages" do
     page.should have_selector 'title', text: full_title('Sign Up')
     click_link "avian"
     page.should have_selector 'title', text: full_title('')
-	end
+  end
 end
