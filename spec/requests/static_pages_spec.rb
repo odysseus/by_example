4e15c8a1e1741bdb2daa88707b5183544e705098
Ryan Case <rmcase@gmail.com>
2013-03-25 19:03:21 -0600

    Minor changes to static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -54,14 +54,14 @@ describe "Static pages" do
 
   describe "About page" do
     before { visit about_path }
-		let(:heading)			{ 'About Us' }
-		let(:page_title) 			{ 'About Us' }
+		let(:heading)			{ 'About' }
+		let(:page_title) 			{ 'About' }
 		it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
-		let(:heading)			{ 'Contact Us' }
+		let(:heading)			{ 'Contact' }
 		let(:page_title)				{ 'Contact' }
 		it_should_behave_like "all static pages"
   end
@@ -69,7 +69,7 @@ describe "Static pages" do
 	it "should have the right links on the layout" do
     visit root_path
     click_link "About"
-    page.should have_selector 'title', text: full_title('About Us')
+    page.should have_selector 'title', text: full_title('About')
     click_link "Help"
     page.should have_selector 'title', text: full_title('Help')
     click_link "Contact"
